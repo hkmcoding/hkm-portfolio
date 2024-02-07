2f06ae2bc38322b1803e1bf55ab66bb17a5109df
Humza M <30579227+hkmcoding@users.noreply.github.com>
2024-02-07 01:46:11 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,17 @@ const Index = () => {
   return (
     <>
       <Head>
-        <title>Humza Mujib | Portfolio</title>
+        <title>HKM | Portfolio</title>
         <meta name="description" content="Humza Mujib is a Frontend Developer in the greater NY area. Visit his portfolio to see some of the projects he has worked on!" />
         <meta property="og:title" content="Humza Mujib | Portfolio"/>
         <meta property="og:url" content="https://hmujib.com"/>
         <meta property="og:image" content="https://hmujib.com/social-share.png"/>
-        <meta property="og:description" content="Humza Mujib is a Frontend Developer in the greater NY area. Visit his portfolio to see some of the projects he has worked on!"/>
+        <meta property="og:description" content="HKM is a Frontend Developer in the greater NY area. Visit his portfolio to see some of the projects he has worked on!"/>
         <meta property="og:type" content="website"/>
-        <meta property="twitter:title" content="Humza Mujib | Portfolio"/>
+        <meta property="twitter:title" content="HKM | Portfolio"/>
         <meta property="twitter:url" content="https://hmujib.com"/>
         <meta property="twitter:image" content="https://hmujib.com/social-share.png"/>
-        <meta property="twitter:description" content="Humza Mujib is a Frontend Developer in the greater NY area. Visit his portfolio to see some of the projects he has worked on!"/>
+        <meta property="twitter:description" content="HKM is a Frontend Developer in the greater NY area. Visit his portfolio to see some of the projects he has worked on!"/>
         <meta property="twitter:type" content="website"/>
       </Head>
       <Container>
